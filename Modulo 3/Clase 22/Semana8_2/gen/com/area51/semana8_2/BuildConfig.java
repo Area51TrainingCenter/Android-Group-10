79bf26e8ca49d2d098ec9c0966723009669e38f0
johannfjs <johannfjs@gmail.com>
2014-11-14 10:39:26 -0500

    Notice

---
+++ Modulo 3/Clase 22/Semana8_2/gen/com/area51/semana8_2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.area51.semana8_2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Modulo 3/Clase 22/Semana8_2/src/com/area51/utils/Constant.java
@@ -0,0 +1,10 @@
+package com.area51.utils;
+
+import com.area51.semana8_2.R;
+
+public class Constant {
+	public static String rutaVideo = "android.resource://com.area51.semana8_2/"
+			+ R.raw.muestra;
+	public static int tiempoTranscurrido = 0;
+	public static boolean reproduccion = true;
+}
